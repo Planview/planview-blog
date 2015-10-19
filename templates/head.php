95a83a0c2ac4cecc51a1cbfa7f36c3fbf47b3af1
Chad Honea <chonea@planview.com>
2015-10-19 12:12:19 -0500

    Google site verification

--- templates/head.php
+++ templates/head.php
@@ -3,6 +3,7 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
+  <meta name="google-site-verification" content="IP1bov3I8uYaZxbfilbKItakmUdQYzYSt_TCm3vrg1w" /> 
   <?php wp_head(); ?>
 <!--[if lt IE 9]>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.js"></script>
