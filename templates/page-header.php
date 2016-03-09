09fe8ccde56adca3104d5e300cc94562cfe45b5e
Chad Honea <chonea@planview.com>
2016-03-09 17:35:45 -0600

    Added category description to category pages

--- templates/page-header.php
+++ templates/page-header.php
@@ -9,12 +9,17 @@
 			$bgimage = wp_get_attachment_image_src( $id, 'large' );
 			$color = get_field('cat_color', $category);
 			$textcolor = get_field('category_text_color', $category);
+		    $description = $category->category_description;
 		?>
-		<div id="cat-banner" style="background-image:url(<?php echo $bgimage[0];?>);">
-			<h1 style="background-color:rgb(<?php echo $color ?>); background-color:rgba(<?php echo $color ?>,.9); color: <?php echo $textcolor; ?>;">
-				<?php single_cat_title(); ?>
-			</h1>
-		</div>
+        <div id="cat-banner" class="col-xs-12" style="background-image:url(<?php echo $bgimage[0];?>);">
+            <h1 style="background-color:rgb(<?php echo $color ?>); background-color:rgba(<?php echo $color ?>,.9); color: <?php echo $textcolor; ?>;">
+                <?php single_cat_title(); ?>
+            </h1>
+        </div>
+        <div id="cat-description" class="col-xs-12">
+            <p><?php echo $description; ?></p>
+            <hr>
+        </div>
 	<?php elseif (is_author()): ?>
 		<?php get_template_part('templates/author-meta'); ?>
 	<?php else: ?>
