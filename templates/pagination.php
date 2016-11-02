fcc4490270fe1b90c57ce629820df9b0c546380e
CORPORATE\sebner <sebner@planview.com>
2016-11-02 15:25:37 -0500

    Added screen reader text to the page numbers and category description below the pagination.

--- templates/pagination.php
+++ templates/pagination.php
@@ -8,11 +8,20 @@ $pagination = paginate_links( array(
 	'format' => '?paged=%#%',
 	'current' => max( 1, get_query_var('paged') ),
 	'total' => $wp_query->max_num_pages,
-	'prev_text' => 'newer',
-	'next_text' => 'older',
+	'prev_text' => 'newer<span class="sr-only"> blog posts</span>',
+	'next_text' => 'older<span class="sr-only"> blog posts</span>',
 	'mid_size' => 2,
 	'type' => 'list',
+	'before_page_number' => '<span class="sr-only">page </span>',
 ) );
 ?>
-
 <div id="pagination"><?php echo $pagination ?></div>
+<?php if (is_category()):
+	$category = get_queried_object();
+	$description = $category->category_description;
+	?>
+<div id="cat-description" class="col-xs-12">
+   <h4 class="text-center"><?php single_cat_title(); ?></h4>
+   <p><?php echo $description; ?></p>
+</div>
+<?php endif; ?>
