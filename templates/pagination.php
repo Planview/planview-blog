74c31843ecbccbf522c9475d9b7665e1bf03c208
CORPORATE\sebner <sebner@planview.com>
2016-12-05 10:33:34 -0600

    Updated the category description styles.

--- templates/pagination.php
+++ templates/pagination.php
@@ -16,12 +16,24 @@ $pagination = paginate_links( array(
 ) );
 ?>
 <div id="pagination"><?php echo $pagination ?></div>
+
 <?php if (is_category()):
 	$category = get_queried_object();
 	$description = $category->category_description;
+  $id = get_field('category_image', $category);
+  $catimage = wp_get_attachment_image_src( $id, 'thumbnail' );
 	?>
-<div id="cat-description" class="col-xs-12">
-   <h4 class="text-center"><?php single_cat_title(); ?></h4>
-   <p><?php echo $description; ?></p>
+<div id="category-meta">
+  <div class="media">
+    <div class="media-left hidden-xs">
+      <img src="<?php echo $catimage[0];?>" class="img-circle" alt="<?php single_cat_title(); ?>" title="<?php single_cat_title(); ?>" >
+    </div>
+    <div class="media-body">
+      <h3 class="media-heading">
+        <?php single_cat_title(); ?>
+      </h3>
+      <p><?php echo $description; ?></p>
+    </div>
+  </div>
 </div>
 <?php endif; ?>
